b7efc56acb2785ea3072500197ee867730b4179c
Manasi_Amazatic <manasi.s@amazatic.com>
2021-09-29 21:14:28 +0530

    Rest code 2 first commit

---
+++ core/serializers.py
@@ -0,0 +1,71 @@
+from rest_framework import fields, serializers
+from .models import (Customer, 
+                    Profession,
+                    DataSheet,
+                    Document)
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ('dtype', 'doc_number','customer')
+
+
+class DataSheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataSheet
+        fields = ('id', 'description', 'historical_data')
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ('id', 'description')
+
+class CustomerSerializer(serializers.ModelSerializer):
+    num_professions = serializers.SerializerMethodField()
+    data_sheet = DataSheetSerializer()
+    professions = ProfessionSerializer(many=True)
+    document_set = DocumentSerializer(many=True,read_only=True)
+
+
+    class Meta:
+        model = Customer
+        fields = ('id','name','address','professions','data_sheet','active','status_message','num_professions')
+
+    def create(self, validated_data):
+        professions = validated_data['professions']
+        del validated_data['professions']      
+
+        document_set = validated_data['document_set']
+        del validated_data['document_set']      
+
+        data_sheet = validated_data['data_sheet']
+        del validated_data['data_sheet']      
+
+        d_sheet = DataSheet.objects.create(**data_sheet)
+        customer = Customer.objects.create(**validated_data)
+        customer.data_sheet = d_sheet
+
+        customer = Customer.objects.create(validated_data)
+
+        for doc in document_set:
+            Document.objects.create(
+                dtype = doc['dtype'],
+                doc_number=doc['doc_number'],
+                customer_id=customer.id
+            )
+
+        for profession in professions:
+            prof = Profession.objects.create(**profession)
+            customer.professions.add(prof)
+        
+        customer.save()
+
+        return customer
+
+    def get_num_professions(self, obj):
+        return obj.num_professions()
+
+    def get_data_sheet(self, obj):
+        return obj.data_sheet.description
+
+
